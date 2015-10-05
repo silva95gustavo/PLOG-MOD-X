8738d1e4e312bf8bd328fc1f4d6c2f908c07a35b
gtugablue <gtugablue@gmail.com>
2015-10-05 22:29:32 +0100

    Board visualization.

--- src/modx.pl
+++ src/modx.pl
@@ -1,30 +1,30 @@
-/* Print */
+/* Visualization */
 
 print_board([H | T]) :-
         print_dashed_line(H), nl,
         print_board_content([H | T]).
 
 print_board_content([H | T]) :-
-        print_board_line(H), nl,
+        print_board_row(H), nl,
         print_dashed_line(H), nl,
         print_board_content(T).
 print_board_content([]).
 
-print_board_line([H | T]) :-
-        write('|'),
-        print_board_line_aux([H | T]).
-print_board_line_aux([H | T]) :-
-        print_piece(H, X),
+print_board_row([H | T]) :-
+        write('|'), print_board_line_aux([H | T], 1), nl,
+        write('|'), print_board_line_aux([H | T], 2), nl,
+        write('|'), print_board_line_aux([H | T], 3).
+print_board_line_aux([[B, P] | T], Line) :-
         write(' '),
-        write(X),
+        print_cell(B, P, Line),
         write(' |'),
-        print_board_line_aux(T).
-print_board_line_aux([]).
+        print_board_line_aux(T, Line).
+print_board_line_aux([], _).
 
 print_piece(H, H).
 
 print_dashed_line([_ | T]) :-
-        write('----'),
+        write('--------'),
         print_dashed_line(T).
 print_dashed_line([]) :- write('-').
 
@@ -36,30 +36,37 @@ piece_to_ascii(3, '^').
 piece_to_ascii(4, ';').
 
 base_to_ascii(-1, ' ').
-base_to_ascii(0, 'x').
+base_to_ascii(0, '=').
 base_to_ascii(1, 'm').
-base_to_ascii(2, 'o').
+base_to_ascii(2, '+').
 base_to_ascii(3, '~').
 base_to_ascii(4, ':').
 
-print_cell(Base, Piece, 1) :-
-        piece_to_ascii(Piece, P),
-        base_to_ascii(Base, B),
-        print(P),
-        print(P),
-        print(B),
-        print(B),
-        print(B),
-        print(P),
-        print(P).
-print_cell(Base, Piece, 2) :-
-        piece_to_ascii(Piece, P),
+print_cell(Base, -1, _) :-
         base_to_ascii(Base, B),
         print(B),
         print(B),
-        print(B),
-        print(P),
         print(B),
         print(B),
         print(B).
-print_cell(Base, Piece, 3) :- print_cell(Base, Piece, 1).
+print_cell(Base, Piece, 1) :-
+        Piece > -1,
+        piece_to_ascii(Piece, P),
+        base_to_ascii(Base, B),
+        print(P),
+        print(B),
+        print(B),
+        print(B),
+        print(P).
+print_cell(Base, Piece, 2) :-
+        Piece > -1,
+        piece_to_ascii(Piece, P),
+        base_to_ascii(Base, B),
+        print(B),
+        print(B),
+        print(P),
+        print(B),
+        print(B).
+print_cell(Base, Piece, 3) :-
+        Piece > -1,
+        print_cell(Base, Piece, 1).
