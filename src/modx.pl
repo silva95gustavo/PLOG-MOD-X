ebe235684c0dfdcae21a70dc4565eecb330a256c
gtugablue <gtugablue@gmail.com>
2015-10-05 21:35:02 +0100

    ...

--- src/modx.pl
+++ src/modx.pl
@@ -27,3 +27,39 @@ print_dashed_line([_ | T]) :-
         write('----'),
         print_dashed_line(T).
 print_dashed_line([]) :- write('-').
+
+piece_to_ascii(-1, ' ').
+piece_to_ascii(0, 'X').
+piece_to_ascii(1, 'M').
+piece_to_ascii(2, 'O').
+piece_to_ascii(3, '^').
+piece_to_ascii(4, ';').
+
+base_to_ascii(-1, ' ').
+base_to_ascii(0, 'x').
+base_to_ascii(1, 'm').
+base_to_ascii(2, 'o').
+base_to_ascii(3, '~').
+base_to_ascii(4, ':').
+
+print_cell(Base, Piece, 1) :-
+        piece_to_ascii(Piece, P),
+        base_to_ascii(Base, B),
+        print(P),
+        print(P),
+        print(B),
+        print(B),
+        print(B),
+        print(P),
+        print(P).
+print_cell(Base, Piece, 2) :-
+        piece_to_ascii(Piece, P),
+        base_to_ascii(Base, B),
+        print(B),
+        print(B),
+        print(B),
+        print(P),
+        print(B),
+        print(B),
+        print(B).
+print_cell(Base, Piece, 3) :- print_cell(Base, Piece, 1).
