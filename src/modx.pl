f8915616635ab871edf3c31c954dc84f5813b13d
gtugablue <gtugablue@gmail.com>
2015-10-05 17:08:19 +0100

    Continued working in print functions.

---
+++ src/modx.pl
@@ -0,0 +1,27 @@
+print_board([H | T]) :-
+        print_dashed_line(H), nl,
+        print_board_content([H | T]).
+
+print_board_content([H | T]) :-
+        print_board_line(H), nl,
+        print_dashed_line(H), nl,
+        print_board_content(T).
+print_board_content([]).
+
+print_board_line([H | T]) :-
+        write('|'),
+        print_board_line_aux([H | T]).
+print_board_line_aux([H | T]) :-
+        print_piece(H, X),
+        write(' '),
+        write(X),
+        write(' |'),
+        print_board_line_aux(T).
+print_board_line_aux([]).
+
+print_piece(H, H).
+
+print_dashed_line([_ | T]) :-
+        write('----'),
+        print_dashed_line(T).
+print_dashed_line([]) :- write('-').
--- src/print.pl
+++
@@ -1,9 +0,0 @@
-imprime([H | T]) :-
-        mostra(H), nl,
-        imprime(T).
-imprime([]).
-mostra([H | T]) :-
-        traduz(H, X), write(X), write('|'),
-        mostra(T).
-mostra([]).
-traduz(H, H).
